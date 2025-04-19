631b00841c21fb4571ea06fed645a172ae6cc5df
The Berufegoru <giftmoobi@gmail.com>
2025-04-19 20:04:56 +0200

    Refactor route registration to use register_routes function

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from .extensions import db, migrate
-from app.routes import routes
+from app.routes import register_routes
 
 def create_app(config_class='app.config.DevConfig'):
     app = Flask(__name__)
@@ -11,6 +11,6 @@ def create_app(config_class='app.config.DevConfig'):
     migrate.init_app(app, db)
 
     # Register Blueprints
-    app.register_blueprint(routes)
+    register_routes(app)
     
     return app
